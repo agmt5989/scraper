1f91086ebaf3819e9f3c54554a6764ec2408ea57
Michael Ajala <agmt5989@gmail.com>
2019-01-18 22:26:02 +0100

    index.js

--- index.js
+++ index.js
@@ -9,4 +9,15 @@ axios.get(url)
     })
     .catch(error => {
         console.log(error);
+    });
+
+let getData = html => {
+    data = [];
+    const $ = cheerio.load(html);
+    $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+        data.push({
+            title: $(elem).text(),
+            link: $(elem).find('a.storylink').attr('href'),
+        });
     })
+}
