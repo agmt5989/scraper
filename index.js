ad25f84f61ae1766847dc6ec2ea1f9a581562dce
Michael Ajala <agmt5989@gmail.com>
2019-01-18 22:21:33 +0100

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+const url = 'https://news.ycombinator.com';
+
+axios.get(url)
+    .then(response => {
+        console.log(response.data);
+    })
